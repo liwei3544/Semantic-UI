e090343cf456b7e742b712bdea3b340bc2f265e0
liwei <354429212@qq.com>
2017-01-22 15:44:02 +0800

    测试提交源码

---
+++ JsonHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace Lottery.Gather
+{
+
+    /// <summary>
+    /// Json格式化帮助类
+    /// </summary>
+    public static class JsonHelper
+    {
+        #region Json反序列化
+        /// <summary>
+        /// 将指定的 JSON 字符串转换为 T 类型的对象
+        /// </summary>
+        /// <typeparam name="T">所生成对象的类型</typeparam>
+        /// <param name="str">要进行反序列化的 JSON 字符串</param>
+        /// <returns>反序列化的对象</returns>
+        public static T JSONDeserialize<T>(this string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            return jss.Deserialize<T>(str);
+        }
+
+        /// <summary>
+        /// 将指定的 JSON 字符串转换为对象图
+        /// </summary>
+        /// <param name="str">要进行反序列化的 JSON 字符串</param>
+        /// <returns>反序列化对象</returns>
+        public static object JSONDeserialize(this string str)
+        {
+            if (str == null)
+                throw new ArgumentNullException("str");
+
+            var jss = new JavaScriptSerializer();
+            return jss.DeserializeObject(str);
+        }
+        #endregion
+
+        #region Json序列化
+        /// <summary>
+        /// 将对象进行JSON格式序列化
+        /// </summary>
+        /// <param name="obj">要序列化的对象</param>
+        /// <returns>JSON字符串</returns>
+        public static string JSONSerialize(this object obj)
+        {
+            var jss = new JavaScriptSerializer();
+            return jss.Serialize(obj);
+        }
+        #endregion
+    }
+}
---
+++ frmMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Lottery.Gather
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
+            this.lblLoadTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(16, 58);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(974, 382);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1006, 453);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://www.hu8r.com/", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Interval = 1000;
+            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
+            // 
+            // lblLoadTime
+            // 
+            this.lblLoadTime.AutoSize = true;
+            this.lblLoadTime.Location = new System.Drawing.Point(1086, 17);
+            this.lblLoadTime.Name = "lblLoadTime";
+            this.lblLoadTime.Size = new System.Drawing.Size(0, 15);
+            this.lblLoadTime.TabIndex = 24;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 453);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lblLoadTime);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Text = "分分彩数据采集";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timerRefresh;
+        private System.Windows.Forms.Label lblLoadTime;
+    }
+}
+
